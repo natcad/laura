7259c165b327f2d5bf0fc36c66ec551580da53cb
natcad <natashacadabon@gmail.com>
2023-04-19 17:23:29 -0300

    3

---
+++ pages/form.php
@@ -0,0 +1,20 @@
+<?php
+
+$nombre=$_POST['nombre'];
+$email=$_POST['email'];
+$textarea=$_POST['textarea'];
+
+$mensaje= 'Este mensaje fue enviado por $nombre \n';
+$mensaje .= 'Su email es: $mail \n';
+$mensaje .= 'Mensaje:  $textarea \n';
+$mensaje .= 'Enviado el ' . date('d/m/Y',time());
+
+$para= 'natashacadabon@gmail.com';
+$asunto='Este mail fue enviado desde lauracadabon.com';
+
+mail($para, $asunto, utf8_decode($mensaje));
+
+header('Location:contactoExito.html');
+
+
+?>
